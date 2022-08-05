732075df71da76d111398cc930366b61515356a2
MarioMH8 <mhidalgomario@gmail.com>
2022-08-05 16:04:49 +0200

    style: removed unused imports

--- lib/spec/domain/value-object/BooleanValueObject.test.ts
+++ lib/spec/domain/value-object/BooleanValueObject.test.ts
@@ -4,21 +4,21 @@ import { InvalidBooleanValueTypeError } from '../../../src/domain/error/InvalidB
 import { BooleanValueObjectMother } from './mother/BooleanValueObjectMother';
 
 describe('BooleanValueObject', () => {
-    test('should create', () => {
-        const vo = BooleanValueObjectMother.create(true);
-        const compare = BooleanValueObjectMother.create(false);
+	test('should create', () => {
+		const vo = BooleanValueObjectMother.create(true);
+		const compare = BooleanValueObjectMother.create(false);
 
-        expect(vo).toBeDefined();
-        expect(vo.value).toBe(true);
-        expect(vo.toString()).toBe('true');
-        expect(vo.isEqualsTo(compare)).toBeFalsy();
-    });
-    test('should throw an error if undefined', () => {
-        const fn = () => BooleanValueObjectMother.invalidWithUndefined();
-        expect(fn).toThrow(InvalidBooleanValueTypeError);
-    });
-    test('should throw an error if invalid type', () => {
-        const fn = () => BooleanValueObjectMother.invalidWithString();
-        expect(fn).toThrow(InvalidBooleanValueTypeError);
-    });
+		expect(vo).toBeDefined();
+		expect(vo.value).toBe(true);
+		expect(vo.toString()).toBe('true');
+		expect(vo.isEqualsTo(compare)).toBeFalsy();
+	});
+	test('should throw an error if undefined', () => {
+		const fn = () => BooleanValueObjectMother.invalidWithUndefined();
+		expect(fn).toThrow(InvalidBooleanValueTypeError);
+	});
+	test('should throw an error if invalid type', () => {
+		const fn = () => BooleanValueObjectMother.invalidWithString();
+		expect(fn).toThrow(InvalidBooleanValueTypeError);
+	});
 });
--- lib/spec/domain/value-object/IdentifierValueObject.test.ts
+++ lib/spec/domain/value-object/IdentifierValueObject.test.ts
@@ -4,18 +4,18 @@ import { InvalidIdentifierValueError } from '../../../src/domain/error/InvalidId
 import { IdentifierValueObjectMother } from './mother/IdentifierValueObjectMother';
 
 describe('IdentifierValueObject', () => {
-    test('should create', () => {
-        const uuid = 'f3cf177a-437b-408a-a967-607891add1df';
-        const vo = IdentifierValueObjectMother.create(uuid);
-        const other = IdentifierValueObjectMother.random();
+	test('should create', () => {
+		const uuid = 'f3cf177a-437b-408a-a967-607891add1df';
+		const vo = IdentifierValueObjectMother.create(uuid);
+		const other = IdentifierValueObjectMother.random();
 
-        expect(vo).toBeDefined();
-        expect(vo.value).toBe(uuid);
-        expect(vo.toString()).toBe('f3cf177a-437b-408a-a967-607891add1df');
-        expect(vo.isEqualsTo(other)).toBeFalsy();
-    });
-    test('should throw an error if invalid', () => {
-        const fn = () => IdentifierValueObjectMother.invalid();
-        expect(fn).toThrow(InvalidIdentifierValueError);
-    });
+		expect(vo).toBeDefined();
+		expect(vo.value).toBe(uuid);
+		expect(vo.toString()).toBe('f3cf177a-437b-408a-a967-607891add1df');
+		expect(vo.isEqualsTo(other)).toBeFalsy();
+	});
+	test('should throw an error if invalid', () => {
+		const fn = () => IdentifierValueObjectMother.invalid();
+		expect(fn).toThrow(InvalidIdentifierValueError);
+	});
 });
