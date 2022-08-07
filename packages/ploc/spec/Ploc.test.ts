e75c19a5ae3e084a48c61a3a48e9fe1e102b7843
MarioMH8 <mhidalgomario@gmail.com>
2022-08-07 17:14:25 +0200

    test: added ploc test

---
+++ packages/ploc/spec/Ploc.test.ts
@@ -0,0 +1,62 @@
+import { describe, expect, test, vi } from 'vitest';
+import { Ploc } from '../src';
+
+interface PlocState {
+	kind: 'LoadedState';
+	value: number;
+}
+
+class MockPloc extends Ploc<PlocState> {
+	constructor(initialState: PlocState) {
+		super(initialState);
+	}
+
+	increment() {
+		this.changeState({
+			...this.state,
+			value: this.state.value + 1,
+		});
+	}
+}
+
+describe('Ploc', () => {
+	test('should works as expected', async () => {
+		const spy = vi.fn<[PlocState], void>();
+		const subscription = (state: PlocState) => spy(state);
+		const ploc = new MockPloc({
+			kind: 'LoadedState',
+			value: 0,
+		});
+
+		expect(ploc.state).toStrictEqual({
+			kind: 'LoadedState',
+			value: 0,
+		});
+
+		ploc.subscribe(subscription);
+
+		ploc.increment();
+
+		const expectedState1 = {
+			kind: 'LoadedState',
+			value: 1,
+		};
+
+		expect(ploc.state).toStrictEqual(expectedState1);
+
+		expect(spy).toHaveBeenCalledWith(expectedState1);
+
+		ploc.unsubscribe(subscription);
+
+		ploc.increment();
+
+		const expectedState2 = {
+			kind: 'LoadedState',
+			value: 2,
+		};
+
+		expect(ploc.state).toStrictEqual(expectedState2);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+	});
+});
