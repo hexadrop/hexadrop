62de80cf8987c71d8103656fc101e9476f9d516a
MarioMH8 <mhidalgomario@gmail.com>
2022-08-16 13:17:33 +0200

    test: added remaining test to DomainNotFoundError

--- packages/core/spec/error/DomainNotFoundError.test.ts
+++ packages/core/spec/error/DomainNotFoundError.test.ts
@@ -3,7 +3,17 @@ import { describe, expect, test } from 'vitest';
 import { DomainNotFoundErrorMother } from './mother/DomainNotFoundErrorMother';
 
 describe('DomainNotFoundError', () => {
-	test('should create from string', () => {
+	test('should create from domain, id', () => {
+		const domain = 'User';
+		const id = UuidMother.random();
+		const expectedError = DomainNotFoundErrorMother.create(domain, id);
+		expect(expectedError.message).toBe(`${domain} with id '${id}' was not found`);
+		expect(expectedError.errorCode).toBe(404);
+		expect(expectedError.code).toBe('HEX(404)');
+		expect(expectedError.name).toBe('DomainNotFoundError');
+		expect(expectedError.stack).toBeDefined();
+	});
+	test('should create from domain, id, param', () => {
 		const domain = 'User';
 		const id = UuidMother.random();
 		const param = 'code';
@@ -14,7 +24,7 @@ describe('DomainNotFoundError', () => {
 		expect(expectedError.name).toBe('DomainNotFoundError');
 		expect(expectedError.stack).toBeDefined();
 	});
-	test('should create from string and code', () => {
+	test('should create from domain, id, param and code', () => {
 		const domain = 'User';
 		const id = UuidMother.random();
 		const param = 'code';
