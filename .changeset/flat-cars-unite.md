1f0e5ab263e8fa0df8e865470939e3fdbcac3d97
MarioMH8 <mhidalgomario@gmail.com>
2023-03-09 15:11:09 +0100

    fix: `IdentifierValueObject` property name

---
+++ .changeset/flat-cars-unite.md
@@ -0,0 +1,5 @@
+---
+"@hexadrop/core": patch
+---
+
+Fix `IdentifierValueObject` property name
--- packages/core/src/value-object/IdentifierValueObject.ts
+++ packages/core/src/value-object/IdentifierValueObject.ts
@@ -7,7 +7,7 @@ const REGEXP = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9
 
 export class IdentifierValueObject extends StringValueObject {
 	constructor(value: string, property?: string) {
-		super(value);
+		super(value, undefined, property);
 		IdentifierValueObject.ensureIsValidUuid(value, property);
 	}
 
