e7e1dea221b59ddec01cf33f71d86bbeffd46909
MarioMH8 <mhidalgomario@gmail.com>
2023-12-26 18:08:58 +0100

    fix(error): remove unused code

--- packages/error/src/invalid-argument.mother.ts
+++ packages/error/src/invalid-argument.mother.ts
@@ -1,16 +1,7 @@
-import { faker } from '@faker-js/faker';
-
 import InvalidArgumentError from './invalid-argument';
 
 export default class InvalidArgumentErrorMother {
 	static create(message: string, name?: string, code?: string): InvalidArgumentError {
 		return new InvalidArgumentError(message, name, code);
-	}
-
-	static random(): InvalidArgumentError {
-		const message = faker.lorem.word();
-		const name = faker.lorem.word();
-
-		return InvalidArgumentErrorMother.create(message, name, 'TST(400)');
 	}
 }
--- packages/error/src/not-found.mother.ts
+++ packages/error/src/not-found.mother.ts
@@ -1,32 +1,7 @@
-import { faker } from '@faker-js/faker';
-
 import DomainNotFoundError from './not-found';
 
 export default class DomainNotFoundErrorMother {
 	static create(domain: string, id: string, param?: string, name?: string, code?: string): DomainNotFoundError {
 		return new DomainNotFoundError(domain, id, param, name, code);
-	}
-
-	static random(): DomainNotFoundError {
-		const domain = faker.lorem.word();
-		const id = crypto.randomUUID();
-		const param = faker.lorem.word();
-
-		return DomainNotFoundErrorMother.create(domain, id, param);
-	}
-
-	static randomWithCode(code: string): DomainNotFoundError {
-		const domain = faker.lorem.word();
-		const id = crypto.randomUUID();
-		const param = faker.lorem.word();
-
-		return DomainNotFoundErrorMother.create(domain, id, param, undefined, code);
-	}
-
-	static randomWithOutParam(): DomainNotFoundError {
-		const domain = faker.lorem.word();
-		const id = crypto.randomUUID();
-
-		return DomainNotFoundErrorMother.create(domain, id);
 	}
 }
