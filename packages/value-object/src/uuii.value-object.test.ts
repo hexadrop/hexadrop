18be74571aa9a1b9aceb8f1d0dce7dafd67993cf
MarioMH8 <mhidalgomario@gmail.com>
2023-12-26 18:09:27 +0100

    test(value-object): improve uuii.value-object tests

--- packages/value-object/src/uuii.value-object.test.ts
+++ packages/value-object/src/uuii.value-object.test.ts
@@ -1,21 +1,40 @@
 import { describe, expect, test } from 'bun:test';
+import UuiiValueObject from './uuii.value-object';
 
 import InvalidUuiiValueError from './uuii.value-object.error';
 import UuiiValueObjectMother from './uuii.value-object.mother';
 
 describe('UuiiValueObject', () => {
-	test('should create', () => {
-		const uuid = 'f3cf177a-437b-408a-a967-607891add1df';
-		const vo = UuiiValueObjectMother.create(uuid);
-		const other = UuiiValueObjectMother.random();
+	describe('constructor()', () => {
+		test('should instantiate', () => {
+			const uuid = 'f3cf177a-437b-408a-a967-607891add1df';
+			const vo = UuiiValueObjectMother.create(uuid);
 
-		expect(vo).toBeDefined();
-		expect(vo.value).toBe(uuid);
-		expect(vo.toString()).toBe('f3cf177a-437b-408a-a967-607891add1df');
-		expect(vo.isEqualsTo(other)).toBeFalsy();
+			expect(vo).toBeDefined();
+			expect(vo.value).toBe(uuid);
+			expect(vo.toString()).toBe('f3cf177a-437b-408a-a967-607891add1df');
+		});
+		test('should throw an error if invalid format', () => {
+			const fn = () => UuiiValueObjectMother.invalid();
+			expect(fn).toThrow(new InvalidUuiiValueError('s'));
+		});
 	});
-	test('should throw an error if invalid', () => {
-		const fn = () => UuiiValueObjectMother.invalid();
-		expect(fn).toThrow(new InvalidUuiiValueError('s'));
+	describe('isEqualsTo()', () => {
+		test('should works as expected', () => {
+			const uuid = 'f3cf177a-437b-408a-a967-607891add1df';
+			const vo1 = UuiiValueObjectMother.create(uuid);
+			const vo2 = UuiiValueObjectMother.create(uuid);
+			const vo3 = UuiiValueObjectMother.random();
+
+			expect(vo1.isEqualsTo(vo2)).toBeTruthy();
+			expect(vo1.isEqualsTo(vo3)).toBeFalsy();
+		});
+	});
+	describe('static random()', () => {
+		test('should works as expected', () => {
+			const random = UuiiValueObject.random();
+
+			expect(random).toBeTruthy();
+		});
 	});
 });
