711d6650c16c1a90e16f90589eb9a066aedd1fae
MarioMH8 <mhidalgomario@gmail.com>
2023-12-28 16:48:53 +0100

    docs(command): include documentation and fix eslint

--- packages/command/src/decorator.ts
+++ packages/command/src/decorator.ts
@@ -1,19 +1,35 @@
 import type { CommandClass } from './command';
-import CommandCommand from './command';
+import Command from './command';
 
-function CommandHandler<E extends CommandCommand>(command: CommandClass<E>): ClassDecorator {
+/**
+ * @constant
+ * @description The metadata key used to define command handlers.
+ */
+const COMMAND_HANDLER_METADATA_KEY = 'command-handler';
+
+/**
+ * @function CommandHandlerDecorator
+ * @description A decorator function to define command handlers.
+ * @param {...CommandClass<CommandType>[]} commands - The commands that the handler can handle.
+ * @returns {ClassDecorator} - A class decorator that defines the command handler.
+ * @template CommandType - The type of the command.
+ */
+function CommandHandlerDecorator<CommandType extends Command>(
+	...commands: CommandClass<CommandType>[]
+): ClassDecorator {
 	return <ClassType extends Function>(target: ClassType): ClassType => {
-		Reflect.defineMetadata('command-handler', true, target);
-
-		const handlers = Reflect.getMetadata<ClassType[]>('command-handlers', command);
-		if (handlers) {
-			handlers.push(target);
+		if ('run' in target.prototype) {
+			for (const command of commands) {
+				Reflect.defineMetadata<ClassType>(COMMAND_HANDLER_METADATA_KEY, target, command);
+			}
 		} else {
-			Reflect.defineMetadata('command-handlers', [target], command);
+			throw new Error('CommandHandler must implements a `run()` method');
 		}
 
 		return target;
 	};
 }
 
-export default CommandHandler;
+export { COMMAND_HANDLER_METADATA_KEY };
+
+export default CommandHandlerDecorator;
