2e3e1b7d6c598b94249a41a6a6a4eed1e42b5762
MarioMH8 <mhidalgomario@gmail.com>
2023-12-28 16:53:27 +0100

    docs(command): include documentation

--- packages/command/src/error/command-not-registered.error.ts
+++ packages/command/src/error/command-not-registered.error.ts
@@ -1,6 +1,14 @@
 import InvalidArgumentError from '@hexadrop/error/invalid-argument';
 
-export class CommandNotRegisteredError extends InvalidArgumentError {
+/**
+ * CommandNotRegisteredError class that extends InvalidArgumentError.
+ * This error is thrown when a command does not have an associated command handler.
+ */
+export default class CommandNotRegisteredError extends InvalidArgumentError {
+	/**
+	 * CommandNotRegisteredError constructor.
+	 * @param {string} command - The command name that does not have an associated command handler.
+	 */
 	constructor(command: string) {
 		super(`The command '${command}' hasn't a command handler associated`, 'CommandNotRegisteredError', 'HEX(500)');
 	}
--- packages/command/src/error/invalid-command.error.ts
+++ packages/command/src/error/invalid-command.error.ts
@@ -1,6 +1,14 @@
-import  DomainError from '@hexadrop/error';
+import DomainError from '@hexadrop/error';
 
-export class InvalidCommandError extends DomainError {
+/**
+ * InvalidCommandError class that extends DomainError.
+ * This error is thrown when a command does not have a 'COMMAND_NAME' static property.
+ */
+export default class InvalidCommandError extends DomainError {
+	/**
+	 * InvalidCommandError constructor.
+	 * It initializes with a specific error message and error code.
+	 */
 	constructor() {
 		super('InvalidCommandError', `The command hasn't an 'COMMAND_NAME' static property`, 'HEX(500)');
 	}
