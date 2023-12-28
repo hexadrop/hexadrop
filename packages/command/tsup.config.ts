2cf3a8f3873562f7dd72a1025c960305fb05b689
MarioMH8 <mhidalgomario@gmail.com>
2023-12-28 16:45:16 +0100

    build(command): optimize build

--- packages/command/tsup.config.ts
+++ packages/command/tsup.config.ts
@@ -5,6 +5,16 @@ export default defineConfig([
 		format: ['esm'],
 		dts: true,
 		clean: true,
-		entry: ['./src/index.ts', './src/bus.ts', './src/decorator.ts'],
+		entry: [
+			'./src/command.ts',
+			'./src/bus.mock-bun.ts',
+			'./src/bus.sync.ts',
+			'./src/bus.ts',
+			'./src/decorator.ts',
+			'./src/command-handlers.ts',
+			'./src/in-memory.command-handlers.ts',
+			'./src/ioc.command-handlers.ts',
+		],
+		minify: true,
 	},
 ]);
