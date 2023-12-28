60e98413abd3fe1415f042c3ea6aaf08f269412f
MarioMH8 <mhidalgomario@gmail.com>
2023-12-28 16:52:08 +0100

    docs(command): include documentation

--- packages/command/src/bus.mock-bun.ts
+++ packages/command/src/bus.mock-bun.ts
@@ -1,64 +1,82 @@
-import { expect, jest } from 'bun:test';
 import Either from '@hexadrop/either';
 import CommandError from '@hexadrop/error';
+import { expect, jest } from 'bun:test';
+
 import type CommandBus from './bus';
-import type { CommandBusCallback, CommandHandler } from './bus';
 import type Command from './command';
-import type { CommandClass } from './command';
 
-export default class MockCommandBus implements CommandBus {
-	readonly dispatchSpy = jest.fn((..._commands: Command[]) =>
-		Promise.resolve(Either.left<void, CommandError>(undefined))
-	);
+/**
+ * @class MockBunCommandBus
+ * @implements CommandBus
+ * @description Class representing a mock command bus for testing in Bun.
+ */
+export default class MockBunCommandBus implements CommandBus {
+	/**
+	 * @property {jest.Mock} dispatchSpy - A mock function for the dispatch method.
+	 */
+	readonly dispatchSpy = jest.fn((..._commands: Command[]) => Promise.resolve(Either.left(undefined)));
 
-	readonly registerSpy = jest.fn(
-		(_command: CommandClass<any>, _handler: CommandBusCallback<any> | CommandHandler<any>) => Promise.resolve()
-	);
-
-	readonly unregisterSpy = jest.fn(
-		(_command: CommandClass<any>, _handler: CommandBusCallback<any> | CommandHandler<any>) => Promise.resolve()
-	);
-
-	private static getDataFromCommand(command: Command) {
+	/**
+	 * @private
+	 * @static
+	 * @method getDataFromCommand
+	 * @description Method to extract data from a command.
+	 * @param {Command} command - The command to extract data from.
+	 * @returns {Object} - The extracted data.
+	 */
+	private static getDataFromCommand(command?: Command): object {
+		if (!command) {
+			return {};
+		}
 		const { commandId: _c, ...attributes } = command;
 
 		return attributes;
 	}
 
+	/**
+	 * @method assertDispatchedCommands
+	 * @description Method to assert that specific commands were dispatched.
+	 * @param {...Command[]} expectedCommands - The expected dispatched commands.
+	 */
 	assertDispatchedCommands(...expectedCommands: Command[]): void {
 		expect(this.dispatchSpy).toHaveBeenCalled();
 		const commandsArr = this.dispatchSpy.mock.calls.flat();
 		expect(commandsArr.length).toEqual(expectedCommands.length);
-		expect(commandsArr.map(e => MockCommandBus.getDataFromCommand(e))).toStrictEqual(
-			expectedCommands.map(e => MockCommandBus.getDataFromCommand(e))
+		expect(commandsArr.map(e => MockBunCommandBus.getDataFromCommand(e))).toStrictEqual(
+			expectedCommands.map(e => MockBunCommandBus.getDataFromCommand(e))
 		);
 	}
 
+	/**
+	 * @method assertLastDispatchedCommand
+	 * @description Method to assert that a specific command was the last one dispatched.
+	 * @param {Command} expectedCommand - The expected last dispatched command.
+	 */
 	assertLastDispatchedCommand(expectedCommand: Command): void {
 		expect(this.dispatchSpy).toHaveBeenCalled();
 		const commandsArr = this.dispatchSpy.mock.calls[this.dispatchSpy.mock.calls.length - 1] ?? [];
 		const command = commandsArr[0];
 		expect(command).toBeDefined();
-		if (command) {
-			expect(MockCommandBus.getDataFromCommand(command)).toStrictEqual(
-				MockCommandBus.getDataFromCommand(expectedCommand)
-			);
-		}
+		expect(MockBunCommandBus.getDataFromCommand(command)).toStrictEqual(
+			MockBunCommandBus.getDataFromCommand(expectedCommand)
+		);
 	}
 
+	/**
+	 * @method assertNotDispatchedCommand
+	 * @description Method to assert that no command was dispatched.
+	 */
 	assertNotDispatchedCommand(): void {
 		expect(this.dispatchSpy).not.toHaveBeenCalled();
 	}
 
+	/**
+	 * @method dispatch
+	 * @description Method to dispatch a command.
+	 * @param {Command} command - The command to be dispatched.
+	 * @returns {Either<void, CommandError> | Promise<Either<void, CommandError>>} - The result of the command dispatch.
+	 */
 	dispatch(command: Command): Either<void, CommandError> | Promise<Either<void, CommandError>> {
-		return this.dispatchSpy(command);
-	}
-
-	register<C extends Command>(command: CommandClass<C>, callback: CommandBusCallback<C> | CommandHandler<C>): void {
-		this.registerSpy(command, callback);
-	}
-
-	unregister<C extends Command>(command: CommandClass<C>, callback: CommandBusCallback<C> | CommandHandler<C>): void {
-		this.unregisterSpy(command, callback);
+		return this.dispatchSpy(command) as Either<void, CommandError> | Promise<Either<void, CommandError>>;
 	}
 }
