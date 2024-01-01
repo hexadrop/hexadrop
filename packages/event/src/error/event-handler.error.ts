fd5c6966533c17b11a8f3ff03fbf985c998bb231
MarioMH8 <mhidalgomario@gmail.com>
2024-01-01 19:30:21 +0100

    feat(event): include event-handler.error.ts

---
+++ packages/event/src/error/event-handler.error.ts
@@ -0,0 +1,15 @@
+import DomainError from '@hexadrop/error';
+
+/**
+ * EventHandlerError is a custom error class that extends DomainError.
+ * It is used to handle errors that occur in the event handler.
+ */
+export default class EventHandlerError extends DomainError {
+	/**
+	 * Constructor for the EventHandlerError class.
+	 * @param {string | Error} message - The error message. If an Error object is passed, its message property is used.
+	 */
+	constructor(message: string | Error) {
+		super('EventHandlerError', typeof message === 'string' ? message : message.message, 'HEX(500)');
+	}
+}
