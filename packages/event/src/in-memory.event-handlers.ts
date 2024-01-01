528eab6dc017ae59be9c325163d49500687b2913
MarioMH8 <mhidalgomario@gmail.com>
2024-01-01 19:37:10 +0100

    docs(event): include documentation

--- packages/event/src/in-memory.event-handlers.ts
+++ packages/event/src/in-memory.event-handlers.ts
@@ -2,14 +2,29 @@ import type { EventBusCallback, EventHandler } from './bus';
 import type { DomainEventClass } from './domain-event';
 import DomainEvent from './domain-event';
 import type { EventHandlers } from './event-handlers';
-
+/**
+ * The InMemoryEventHandlers class implements the EventHandlers interface.
+ * It provides in-memory storage and management of event handlers.
+ */
 export default class InMemoryEventHandlers implements EventHandlers {
+	/**
+	 * A private map that stores event handlers.
+	 */
 	private readonly map: Map<string, (EventBusCallback<any> | EventHandler<any>)[]>;
 
-	constructor(map?: Map<string, EventBusCallback[]>) {
-		this.map = map ?? new Map<string, EventBusCallback[]>();
+	/**
+	 * The constructor initializes the map.
+	 */
+	constructor() {
+		this.map = new Map<string, EventBusCallback[]>();
 	}
 
+	/**
+	 * The 'register' method registers an event handler for a specific event.
+	 *
+	 * @param event - The event for which to register the handler.
+	 * @param handler - The handler to be registered for the event. It can be an 'EventBusCallback' or an instance of 'EventHandler'.
+	 */
 	register<E extends DomainEvent>(event: DomainEventClass<E>, handler: EventBusCallback<E> | EventHandler<E>): void {
 		const callbacks: (EventBusCallback<E> | EventHandler<E>)[] = this.map.get(event.EVENT_NAME) ?? [];
 		const exists = callbacks.find(v => v === handler);
@@ -20,6 +35,12 @@ export default class InMemoryEventHandlers implements EventHandlers {
 		this.map.set(event.EVENT_NAME, callbacks);
 	}
 
+	/**
+	 * The 'search' method searches for event handlers for a specific event.
+	 *
+	 * @param event - The event for which to search the handlers. It can be a 'DomainEventClass' or an instance of 'DomainEvent'.
+	 * @returns An array of 'EventBusCallback' which are the handlers for the provided event.
+	 */
 	search<E extends DomainEvent>(event: DomainEventClass<E> | E): EventBusCallback<E>[] {
 		let handler: (EventBusCallback<E> | EventHandler<E>)[] = [];
 		let eventName: string | undefined = undefined;
@@ -44,6 +65,12 @@ export default class InMemoryEventHandlers implements EventHandlers {
 		});
 	}
 
+	/**
+	 * The 'unregister' method unregisters an event handler for a specific event.
+	 *
+	 * @param event - The event for which to unregister the handler.
+	 * @param handler - The handler to be unregistered for the event. It can be an 'EventBusCallback' or an instance of 'EventHandler'.
+	 */
 	unregister<E extends DomainEvent>(
 		event: DomainEventClass<E>,
 		handler: EventBusCallback<E> | EventHandler<E>
