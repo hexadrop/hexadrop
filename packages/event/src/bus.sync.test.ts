27c6fe33d745a5c7335eeac8e0dba5f540c7317e
MarioMH8 <mhidalgomario@gmail.com>
2024-01-01 19:41:50 +0100

    test(event): refactor tests

--- packages/event/src/bus.sync.test.ts
+++ packages/event/src/bus.sync.test.ts
@@ -1,12 +1,14 @@
 import Either from '@hexadrop/either';
 import DomainError from '@hexadrop/error';
-import { describe, expect, jest, test } from 'bun:test';
+import { beforeEach, describe, expect, jest, test } from 'bun:test';
 
-import type { EventBusCallback, EventHandler } from './bus';
-import EventBusHandlers from './bus.handlers';
+import type { EventHandler } from './bus';
+import EventBus from './bus';
 import SyncEventBus from './bus.sync';
 import type { DomainEventParams } from './domain-event';
 import DomainEvent from './domain-event';
+import { EventHandlerError } from './error';
+import InMemoryEventHandlers from './in-memory.event-handlers';
 
 class HandlerError extends DomainError {
 	constructor() {
@@ -14,33 +16,12 @@ class HandlerError extends DomainError {
 	}
 }
 
-const handler1Spy = jest.fn((_: DomainEvent) => Either.left<void, DomainError>(undefined));
-const handler2Spy = jest.fn((_: DomainEvent) => Either.left<void, DomainError>(undefined));
-const handler3Spy = jest.fn((_: DomainEvent) => Either.left<void, DomainError>(undefined));
-const handler4Spy = jest.fn((_: DomainEvent) => Promise.resolve(Either.left<void, DomainError>(undefined)));
-const handler5Spy = jest.fn((_: DomainEvent) => Either.right<void, DomainError>(new HandlerError()));
-const handler6Spy = jest.fn((_: DomainEvent) => Promise.resolve(Either.right<void, DomainError>(new HandlerError())));
-
-const callback1Spy = jest.fn((_: DomainEvent) => Either.left<void, DomainError>(undefined));
-const callback2Spy = jest.fn((_: DomainEvent) => Promise.resolve(Either.left<void, DomainError>(undefined)));
-const callback7Spy = jest.fn((_: DomainEvent) => Either.right<void, DomainError>(new HandlerError()));
-const callback8Spy = jest.fn((_: DomainEvent) => Promise.resolve(Either.right<void, DomainError>(new HandlerError())));
-
-function callback1(event: DomainEvent) {
-	return callback1Spy(event);
-}
-
-async function callback2(event: DomainEvent) {
-	return callback2Spy(event);
-}
-
-function callback7(event: DomainEvent) {
-	return callback7Spy(event);
-}
-
-async function callback8(event: DomainEvent) {
-	return callback8Spy(event);
-}
+const event1Handler1Spy = jest.fn((_: DomainEvent) => Either.right<DomainError, void>(undefined));
+const event1Handler2Spy = jest.fn((_: DomainEvent) => Either.right<DomainError, void>(undefined));
+const event1Handler3Spy = jest.fn((_: DomainEvent) => Either.left<DomainError, void>(new HandlerError()));
+const event1Handler4Spy = jest.fn((_: DomainEvent) => {
+	throw new HandlerError();
+});
 
 class Event1 extends DomainEvent {
 	static override EVENT_NAME = 'Event1';
@@ -52,164 +33,84 @@ class Event1 extends DomainEvent {
 
 class Event1Handler implements EventHandler<Event1> {
 	run(event: Event1) {
-		return handler1Spy(event);
+		return event1Handler1Spy(event);
 	}
 }
 
-class Event3Handler implements EventHandler<Event1> {
-	run(event: Event1): Either<void, DomainError> {
-		return handler3Spy(event);
-	}
-}
+describe('SyncEventBus', () => {
+	let bus: EventBus;
+	beforeEach(() => {
+		event1Handler1Spy.mockClear();
+		event1Handler2Spy.mockClear();
+		event1Handler3Spy.mockClear();
+	});
+	describe('publish()', () => {
+		test('should works with simple registered event handler', async () => {
+			const info = new InMemoryEventHandlers();
+			info.register(Event1, new Event1Handler());
+			bus = new SyncEventBus(info);
 
-class Event2 extends DomainEvent {
-	static override EVENT_NAME = 'Event2';
+			const either = await bus.publish(new Event1({ aggregateId: '1' }));
 
-	constructor({ eventId, occurredOn, relatedId, aggregateId }: DomainEventParams<Event2>) {
-		super(Event2.EVENT_NAME, { eventId, aggregateId, occurredOn, relatedId });
-	}
-}
+			expect(event1Handler1Spy).toHaveBeenCalledTimes(1);
+			expect(either.isRight()).toBe(true);
+		});
+		test('should works with simple registered callbacks', async () => {
+			const info = new InMemoryEventHandlers();
+			info.register(Event1, event1Handler2Spy);
+			bus = new SyncEventBus(info);
 
-class Event2Handler implements EventHandler<Event2> {
-	run(event: Event2): Either<void, DomainError> {
-		return handler2Spy(event);
-	}
-}
+			const either = await bus.publish(new Event1({ aggregateId: '1' }));
 
-class Event4 extends DomainEvent {
-	static override EVENT_NAME = 'Event4';
+			expect(event1Handler2Spy).toHaveBeenCalledTimes(1);
+			expect(either.isRight()).toBe(true);
+		});
+		test('should works with multiple registered callbacks', async () => {
+			const info = new InMemoryEventHandlers();
+			info.register(Event1, new Event1Handler());
+			info.register(Event1, event1Handler2Spy);
+			bus = new SyncEventBus(info);
 
-	constructor({ eventId, occurredOn, relatedId, aggregateId }: DomainEventParams<Event4>) {
-		super(Event4.EVENT_NAME, { eventId, aggregateId, occurredOn, relatedId });
-	}
-}
+			const either = await bus.publish(new Event1({ aggregateId: '1' }));
 
-class Event4Handler implements EventHandler<Event4> {
-	async run(event: Event4): Promise<Either<void, DomainError>> {
-		return handler4Spy(event);
-	}
-}
+			expect(event1Handler1Spy).toHaveBeenCalledTimes(1);
+			expect(event1Handler2Spy).toHaveBeenCalledTimes(1);
+			expect(either.isRight()).toBe(true);
+		});
+		test('should works with handler with errors', async () => {
+			const info = new InMemoryEventHandlers();
+			info.register(Event1, event1Handler3Spy);
+			bus = new SyncEventBus(info);
 
-class Event5 extends DomainEvent {
-	static override EVENT_NAME = 'Event5';
+			const either = await bus.publish(new Event1({ aggregateId: '1' }));
 
-	constructor({ eventId, occurredOn, relatedId, aggregateId }: DomainEventParams<Event5>) {
-		super(Event5.EVENT_NAME, { eventId, aggregateId, occurredOn, relatedId });
-	}
-}
+			expect(event1Handler3Spy).toHaveBeenCalledTimes(1);
+			expect(either.isLeft()).toBe(true);
+			expect(either.getLeft()).toStrictEqual(new HandlerError());
+		});
+		test('should works with multiple registered callbacks with some errors', async () => {
+			const info = new InMemoryEventHandlers();
+			info.register(Event1, event1Handler2Spy);
+			info.register(Event1, event1Handler3Spy);
+			bus = new SyncEventBus(info);
 
-class Event5Handler implements EventHandler<Event5> {
-	run(event: Event5): Either<void, DomainError> {
-		return handler5Spy(event);
-	}
-}
+			const either = await bus.publish(new Event1({ aggregateId: '1' }));
 
-class Event6 extends DomainEvent {
-	static override EVENT_NAME = 'Event6';
+			expect(event1Handler2Spy).toHaveBeenCalledTimes(1);
+			expect(event1Handler3Spy).toHaveBeenCalledTimes(1);
+			expect(either.isLeft()).toBe(true);
+			expect(either.getLeft()).toStrictEqual(new HandlerError());
+		});
+		test('should works with un handle error', async () => {
+			const info = new InMemoryEventHandlers();
+			info.register(Event1, event1Handler4Spy);
+			bus = new SyncEventBus(info);
 
-	constructor({ eventId, occurredOn, relatedId, aggregateId }: DomainEventParams<Event6>) {
-		super(Event6.EVENT_NAME, { eventId, aggregateId, occurredOn, relatedId });
-	}
-}
+			const either = await bus.publish(new Event1({ aggregateId: '1' }));
 
-class Event6Handler implements EventHandler<Event6> {
-	async run(event: Event6): Promise<Either<void, DomainError>> {
-		return handler6Spy(event);
-	}
-}
-
-class Event7 extends DomainEvent {
-	static override EVENT_NAME = 'Event7';
-
-	constructor({ eventId, occurredOn, relatedId, aggregateId }: DomainEventParams<Event6>) {
-		super(Event7.EVENT_NAME, { eventId, aggregateId, occurredOn, relatedId });
-	}
-}
-
-class Event8 extends DomainEvent {
-	static override EVENT_NAME = 'Event8';
-
-	constructor({ eventId, occurredOn, relatedId, aggregateId }: DomainEventParams<Event8>) {
-		super(Event8.EVENT_NAME, { eventId, aggregateId, occurredOn, relatedId });
-	}
-}
-
-describe('InMemoryEventBus', () => {
-	test('should works as expected', async () => {
-		const event1 = new Event1({ eventId: '1', aggregateId: '1' });
-		const event2 = new Event2({ eventId: '2', aggregateId: '2' });
-		const event4 = new Event4({ eventId: '4', aggregateId: '4' });
-		const event5 = new Event5({ eventId: '5', aggregateId: '5' });
-		const event6 = new Event6({ eventId: '6', aggregateId: '6' });
-		const event7 = new Event7({ eventId: '7', aggregateId: '7' });
-		const event8 = new Event8({ eventId: '8', aggregateId: '8' });
-		const handler1 = new Event1Handler();
-		const handler2 = new Event2Handler();
-		const handler3 = new Event3Handler();
-		const handler4 = new Event4Handler();
-		const handler5 = new Event5Handler();
-		const handler6 = new Event6Handler();
-		const map = new Map<string, EventBusCallback[]>();
-		map.set(Event4.EVENT_NAME, [handler4.run.bind(handler4)]);
-		map.set(Event5.EVENT_NAME, [handler5.run.bind(handler5)]);
-		map.set(Event6.EVENT_NAME, [handler6.run.bind(handler6)]);
-		const info = new EventBusHandlers(map);
-		const bus = new SyncEventBus(info);
-
-		await bus.subscribe(Event1, handler1);
-		await bus.subscribe(Event1, callback1);
-
-		await bus.publish(event1, event2);
-
-		expect(handler1Spy.mock.calls[0]).toStrictEqual([event1]);
-		expect(handler2Spy).toHaveBeenCalledTimes(0);
-		expect(handler3Spy).toHaveBeenCalledTimes(0);
-		expect(handler4Spy).toHaveBeenCalledTimes(0);
-
-		expect(callback1Spy.mock.calls[0]).toStrictEqual([event1]);
-		expect(callback2Spy).toHaveBeenCalledTimes(0);
-		expect(callback7Spy).toHaveBeenCalledTimes(0);
-		expect(callback8Spy).toHaveBeenCalledTimes(0);
-
-		await bus.unsubscribe(Event1, handler1);
-		await bus.unsubscribe(Event1, callback1);
-
-		await bus.publish(event1, event2);
-
-		expect(handler1Spy).toHaveBeenCalledTimes(1);
-		expect(handler2Spy).toHaveBeenCalledTimes(0);
-		expect(handler3Spy).toHaveBeenCalledTimes(0);
-		expect(handler4Spy).toHaveBeenCalledTimes(0);
-
-		expect(callback1Spy).toHaveBeenCalledTimes(1);
-		expect(callback2Spy).toHaveBeenCalledTimes(0);
-		expect(callback7Spy).toHaveBeenCalledTimes(0);
-		expect(callback8Spy).toHaveBeenCalledTimes(0);
-
-		await bus.subscribe(Event1, handler1);
-		await bus.subscribe(Event2, handler2);
-		await bus.subscribe(Event1, handler3);
-
-		await bus.subscribe(Event1, callback1);
-		await bus.subscribe(Event2, callback2);
-		await bus.subscribe(Event7, callback7);
-		await bus.subscribe(Event8, callback8);
-
-		await bus.publish(event1, event2, event4);
-
-		expect(handler1Spy).toHaveBeenCalledTimes(2);
-		expect(handler2Spy).toHaveBeenCalledTimes(1);
-		expect(handler3Spy).toHaveBeenCalledTimes(1);
-		expect(handler4Spy).toHaveBeenCalledTimes(1);
-
-		expect(callback1Spy).toHaveBeenCalledTimes(2);
-		expect(callback2Spy).toHaveBeenCalledTimes(1);
-		expect(callback7Spy).toHaveBeenCalledTimes(0);
-		expect(callback8Spy).toHaveBeenCalledTimes(0);
-
-		expect(async () => await bus.publish(event5)).toThrow(new HandlerError());
-		expect(async () => await bus.publish(event6)).toThrow(new HandlerError());
-		expect(async () => await bus.publish(event7)).toThrow(new HandlerError());
-		expect(async () => await bus.publish(event8)).toThrow(new HandlerError());
+			expect(event1Handler4Spy).toHaveBeenCalledTimes(1);
+			expect(either.isLeft()).toBe(true);
+			expect(either.getLeft()).toStrictEqual(new EventHandlerError('msg mondongo'));
+		});
 	});
 });
