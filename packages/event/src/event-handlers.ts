afcc8ced17bcba43268986bcbad3f15a998a9da2
MarioMH8 <mhidalgomario@gmail.com>
2024-01-01 19:36:57 +0100

    docs(event): include documentation

--- packages/event/src/event-handlers.ts
+++ packages/event/src/event-handlers.ts
@@ -1,7 +1,17 @@
 import type { EventBusCallback } from '@hexadrop/event/bus';
 import type { DomainEventClass } from '@hexadrop/event/domain-event';
 import DomainEvent from '@hexadrop/event/domain-event';
-
+/**
+ * The EventHandlers interface defines the structure for event handlers.
+ * It contains a single method 'search' which is used to search for event handlers.
+ */
 export interface EventHandlers {
+	/**
+	 * The 'search' method is used to search for event handlers.
+	 * It accepts an event of type 'DomainEventClass' or 'DomainEvent' and returns an array of 'EventBusCallback'.
+	 *
+	 * @param event - The event for which to search the handlers. It can be a 'DomainEventClass' or an instance of 'DomainEvent'.
+	 * @returns An array of 'EventBusCallback' which are the handlers for the provided event.
+	 */
 	search<E extends DomainEvent>(event: DomainEventClass<E> | E): EventBusCallback<E>[];
 }
