77d9a1d42ffa64dd35a5965fe5137e98b5027cd5
MarioMH8 <mhidalgomario@gmail.com>
2024-01-01 19:31:01 +0100

    feat(event): include error exports

---
+++ packages/event/src/error/index.ts
@@ -0,0 +1,5 @@
+import EventHandlerError from './event-handler.error';
+import EventNotRegisteredError from './event-not-registered.error';
+import InvalidEventError from './invalid-event.error';
+
+export { EventHandlerError, EventNotRegisteredError, InvalidEventError };
