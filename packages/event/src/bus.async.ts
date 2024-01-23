9cee0bc0b2dabb94b13b8ad93ac72e07d7455c54
MarioMH8 <mhidalgomario@gmail.com>
2024-01-23 12:11:28 +0100

    feat: add an async event-bus

---
+++ packages/event/src/bus.async.ts
@@ -0,0 +1,75 @@
+import Either from '@hexadrop/either';
+import type DomainError from '@hexadrop/error';
+import PQueue from 'p-queue';
+
+import EventBus from './bus';
+import DomainEvent from './domain-event';
+import { EventHandlerError } from './error';
+import EventHandlers from './event-handlers';
+
+/**
+ * AsyncEventBus is a class that extends the abstract EventBus class.
+ * It provides an asynchronous implementation of the EventBus.
+ *
+ * @class
+ * @extends {EventBus}
+ */
+export default class AsyncEventBus extends EventBus {
+	/**
+	 * The event handlers for the bus.
+	 *
+	 * @private
+	 * @type {EventHandlers}
+	 */
+	private readonly info: EventHandlers;
+
+	/**
+	 * The queue for the bus.
+	 *
+	 * @private
+	 * @type {PQueue}
+	 */
+	private readonly queue: PQueue;
+
+	/**
+	 * Creates an instance of AsyncEventBus.
+	 *
+	 * @param {EventHandlers} info - The event handlers for the bus.
+	 * @param {number} [concurrency=5] - The maximum number of concurrent handlers.
+	 */
+	constructor(info: EventHandlers, concurrency = 5) {
+		super();
+		this.info = info;
+		this.queue = new PQueue({ concurrency });
+	}
+
+	/**
+	 * @async
+	 * @param {...DomainEvent[]} events - The domain events to publish.
+	 * @returns {Either<DomainError, void>}
+	 */
+	publish(...events: DomainEvent[]): Either<DomainError, void> {
+		for (const e of events) {
+			const handlers = this.info.search(e);
+			for (const handler of handlers) {
+				void this.queue.add(
+					() =>
+						new Promise<Either<DomainError, void>>(resolve => {
+							try {
+								const returnValue = handler(e);
+								if (returnValue instanceof Promise) {
+									void returnValue.then(e => resolve(e));
+								} else {
+									resolve(returnValue);
+								}
+							} catch (e) {
+								resolve(Either.left(new EventHandlerError(e as Error)));
+							}
+						})
+				);
+			}
+		}
+
+		return Either.right(undefined);
+	}
+}
