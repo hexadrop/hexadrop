2073da260873a8f21164f54f70f589e16fbfd2c8
MarioMH8 <mhidalgomario@gmail.com>
2024-05-09 16:23:59 +0200

    chore: update dependency @hexatool/eslint-config to v4.0.0-beta.7

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,3 @@
 import { default as hexatool } from '@hexatool/eslint-config';
 
-export default hexatool({
-	formatters: true,
-	typescript: {
-		overrides: {
-			'typescript/ban-types': 'off',
-			'typescript/no-explicit-any': 'off',
-			'typescript/no-invalid-void-type': 'off',
-		},
-		tsconfigPath: './tsconfig.json',
-	},
-});
+export default hexatool();
